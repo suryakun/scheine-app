87dafc18dd15a5dc8b3f6661d22e1ead0d3d2222
Surya Surakhman <me@suryasurakhman.com>
2024-09-14 23:24:25 +0700

    Add navigation menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { UserPage } from './components/user-page';
 import { DoctorPage } from './components/doctor-page';
+import { cn } from './lib/utils';
 
 const queryClient = new QueryClient();
 
@@ -11,14 +20,49 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <div className="container mx-auto p-4">
-          <nav className="mb-4">
-            <ul className="flex space-x-4">
-              <li><Link to="/" className="text-blue-500 hover:text-blue-700">Home</Link></li>
-              <li><Link to="/users" className="text-blue-500 hover:text-blue-700">Patient Data</Link></li>
-              <li><Link to="/doctors" className="text-blue-500 hover:text-blue-700">Doctor Data</Link></li>
-            </ul>
-          </nav>
-
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    cn(
+                      navigationMenuTriggerStyle(),
+                      isActive ? "bg-accent text-accent-foreground" : ""
+                    )
+                  }
+                >
+                  Home
+                </NavLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavLink
+                  to="/users"
+                  className={({ isActive }) =>
+                    cn(
+                      navigationMenuTriggerStyle(),
+                      isActive ? "bg-accent text-accent-foreground" : ""
+                    )
+                  }
+                >
+                  Patient
+                </NavLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavLink
+                  to="/doctors"
+                  className={({ isActive }) =>
+                    cn(
+                      navigationMenuTriggerStyle(),
+                      isActive ? "bg-accent text-accent-foreground" : ""
+                    )
+                  }
+                >
+                  Doctor
+                </NavLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
           <Routes>
             <Route path="/" element={<div />} />
             <Route path="/users" element={<UserPage />} />
